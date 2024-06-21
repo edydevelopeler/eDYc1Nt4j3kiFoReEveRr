0b2195f5fe0106c9dc60e1e84db7b9d528f73ff8
Edy Dev <152673375+edydevelopeler@users.noreply.github.com>
2024-06-21 18:45:47 +0700

    ngentot lu

--- jeki.sh
+++ jeki.sh
@@ -207,7 +207,7 @@ apt-get install libio-socket-inet6-perl libsocket6-perl libcrypt-ssleay-perl lib
 timedatectl set-timezone Asia/Jakarta;
 
 #Install Marzban
-sudo bash -c "$(curl -sL https://github.com/edydevelopeler/Marzban-scripts/raw/master/marzban.sh)" @ install
+sudo bash -c "$(curl -sL https://github.com/GawrAme/Marzban-scripts/raw/master/marzban.sh)" @ install
 
 #Install Subs
 wget -N -P /opt/marzban  https://cdn.jsdelivr.net/gh/lunoxxdev/marhabantemplet@main/template-01/index.html
