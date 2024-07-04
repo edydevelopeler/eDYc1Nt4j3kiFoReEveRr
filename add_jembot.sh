d38909fc9a0a154340cd8b2cf9d4dbdf9d6d40db
Edy Dev <152673375+edydevelopeler@users.noreply.github.com>
2024-07-04 22:16:33 +0700

    ngentot lu

---
+++ add_jembot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Prompt for the Telegram API token and admin ID
+read -p "Masukkan API key bot: " TELEGRAM_API_TOKEN
+read -p "Masukkan chat ID: " TELEGRAM_ADMIN_ID
+
+# Define the path to the .env file
+ENV_FILE="/opt/marzban/.env"
+
+# Modify the .env file
+sed -i "s|# TELEGRAM_API_TOKEN = .*|TELEGRAM_API_TOKEN = \"$TELEGRAM_API_TOKEN\"|g" $ENV_FILE
+sed -i "s|# TELEGRAM_ADMIN_ID = .*|TELEGRAM_ADMIN_ID = $TELEGRAM_ADMIN_ID|g" $ENV_FILE
+
+echo "API key bot dan chat ID telah berhasil ditambahkan ke $ENV_FILE"
